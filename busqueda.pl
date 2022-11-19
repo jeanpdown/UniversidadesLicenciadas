bfec494d1eec4f0a4de80a885b70521803517064
piero <pierito2000huamani@gmail.com>
2022-11-19 00:08:38 -0500

    agrege impresion si coincide con la busqueda

--- busqueda.pl
+++ busqueda.pl
@@ -21,15 +21,16 @@ HTML
 
 sub imprimirResultados{
     my $columna=$_[0];
-    my $busqueda=$_[1];
+    my $busq=$_[1];
     open(IN,"UniversidadesLicenciadas.csv") or die"ERROR";
 
         while(my $line = <IN>){
             if($line =~ /^([^\|]+)\|([^\|]+)\|([^\|]+)\|([^\|]+)\|([^\|]+)\|([^\|]+)\|([^\|]+)\|([^\|]+)\|([^\|]+)\|([^\|]+)\|([^\|]+)\|([^\|]+)\|([^\|]+)\|([^\|]+)\|([^\|]+)\|([^\|]+)\|([^\|]+)\|([^\|]+)\|([^\|]+)\|([^\|]+)\|([^\|]+)\|([^\|]+)\|(.+)/){
+                if($$columna =~ /$busq/){
+                    print $line;
 
-
+                }
             }
-
         }
     close(IN);
 }
