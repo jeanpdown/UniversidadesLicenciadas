b51b823dfd94146c90924f189a294c3d6b4b8c8b
piero <pierito2000huamani@gmail.com>
2022-11-19 12:32:23 -0500

    termine generador expresion regular

--- busqueda.pl
+++ busqueda.pl
@@ -21,22 +21,39 @@ my $q =CGI->new;
 my $busqueda=$q->param("busqueda");
 my $opcion=$q->param("opcion");
 
+generadorExpresionRegular(2,$busqueda);
 
 if($opcion eq "Nombre"){
-    imprimirResultados(1,$busqueda);
+    #imprimirResultados(1,$busqueda);
 }
 
 
 sub imprimirResultados{
     my $columna=$_[0];
-    my $busq=$_[1];
-    print "la busqueda es $busq";
-    open(IN,"UniversidadesLicenciadas.csv") or die"ERROR";
+    
+    # print "la busqueda es $busq";
+    # open(IN,"UniversidadesLicenciadas.csv") or die"ERROR";
 
-        while(my $line = <IN>){
-            if($line =~ /^([^\|]+)\|(.*$busq.*)\|(.+)/){   #([^\|]+)
-                print $line;
-            }
-        }
-    close (IN);
+    #     while(my $line = <IN>){
+    #         if($line =~ /^([^\|]+)\|(.*$busq.*)\|(.+)/){   #([^\|]+)
+    #             print $line;
+    #         }
+    #     }
+    # close (IN);
+}
+
+sub generadorExpresionRegular{
+    my $columna=$_[0];
+    my $busq=$_[1];
+    my $expresion="^";
+    
+    for(my $i=1;$i<$columna;$i++){
+        $expresion.="([^\|]+)\|";
+
+    }
+    
+    $expresion.="(.*$busq.*)\|(.+)/";
+    print "expre $expresion\n";
+
+    return $expresion;
 }
