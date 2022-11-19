48fd75d2be45cc69ba9b8e92b4cc0506b86c2618
piero <pierito2000huamani@gmail.com>
2022-11-18 23:56:43 -0500

    agrege la cabecera al html de perl

--- busqueda.pl
+++ busqueda.pl
@@ -4,3 +4,15 @@ use CGI;
 use warnings;
 use utf8;
 
+print "Content-type: text/html\n\n"
+print <<HTML;
+<!DOCTYPE html>
+<html>
+<head>
+    
+    <link rel="stylesheet" type="text/css" href="../estilos.css">
+    <title>Búsquedas Universidades Licenciadas </title>
+</head>
+<body>
+    <div >
+HTML
